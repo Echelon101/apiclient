b7f964aa46bc83798fdbbcec210a62f4bc756b71
Echelon101 <timomrx1@gmail.com>
2017-09-28 10:32:43 +0200

    Added DatabaseClient

---
+++ DatabaseClient/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DatabaseClient
+{
+    public class Client
+    {
+        private MySqlConnection sqlConnection;
+        private string connectionString;
+
+        public Client(string server, string uid, string password, string database)
+        {
+            this.connectionString = "server=" + server + ";uid="+ uid + ";pwd=" + password + ";database=" + database;
+        }
+        public void Connector()
+        {
+            this.sqlConnection = new MySqlConnection();
+            this.sqlConnection.ConnectionString = this.connectionString;
+            this.sqlConnection.Open();
+        }
+    }
+}
