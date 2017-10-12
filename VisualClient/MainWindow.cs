7d65b56bdf9f9578a97d06c06e3661efb63742ef
Echelon101 <timomrx1@gmail.com>
2017-10-12 12:54:45 +0200

    Started to work on Visual Frontend

---
+++ VisualClient/MainWindow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DatabaseClient;
+
+namespace VisualClient
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Client client = new Client(textBox1.Text, textBox2.Text, textBox3.Text, "");
+            if (!client.CheckConnection())
+            {
+                label5.Visible = true;
+                label5.Text = "Login Failed";
+            }else if (client.CheckConnection())
+            {
+                label5.Visible = true;
+                label5.Text = "Login Successful";
+
+                List<string> schemes = client.GetSchemes();
+
+                foreach (string scheme in schemes)
+                {
+                    comboBox1.Items.Add(new ListViewItem(scheme));
+                }
+            }
+            
+        }
+    }
+}
---
+++ VisualClient/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualClient
+{
+    class Utilities
+    {
+        private MainWindow _mainWindow;
+
+        public Utilities(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+    }
+}
