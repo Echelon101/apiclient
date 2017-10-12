8b55153e913b7c14c06d112827988bb5875e063c
Echelon101 <timomrx1@gmail.com>
2017-10-12 12:55:28 +0200

    Started Testing with mysql

---
+++ DatabaseClientTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseClient;
+using System.Data;
+
+namespace DatabaseClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client("localhost", "root", "", "export");
+            /*
+            List<string> fields = client.SelectQuery("*", "exportlog");
+
+            foreach (string field in fields)
+            {
+                Console.WriteLine(field);
+            }
+
+            */
+
+            List<string> tables = client.GetTables();
+
+            foreach (string table in tables)
+            {
+                Console.WriteLine(table);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
